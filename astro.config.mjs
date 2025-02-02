59c2d373cf487248ed3386ec2785110f6c9ec1ba
Theotime <tmamou@etu.uqac.ca>
2025-02-02 11:14:15 -0500

    modif deploy yml et config astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,4 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
     site: 'https://theotimem.github.io/', // Mets l'URL de ton GitHub Pages
     base: '/portfolio/', // Mets le nom de ton repo ici
-    output: 'static',
-    integrations: [sitemap()]
 });
