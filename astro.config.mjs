122fb8de844b7e2ec5abd5e16fbdbe92cb233287
Theotime <tmamou@etu.uqac.ca>
2025-02-02 01:17:56 -0500

    modif github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import sitemap from '@astrojs/sitemap';
+import { githubPages } from 'astro/github';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://TheotimeM.github.io/', // Mets l'URL de ton GitHub Pages
+    base: '/portfolio/', // Mets le nom de ton repo ici
+    output: 'static',
+    adapter: githubPages(),
+    integrations: [sitemap()],
+});
